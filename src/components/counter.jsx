044fb4711a0e4bc4b5a8c3dd870ab2528095a5fb
Daniel <danielpires6@hotmail.com>
2019-02-26 19:27:38 -0300

    Finalizando CRUD

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,40 +1,34 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        value: this.props.value,
-    };
-
-    handleIncrement = () => {
-        this.setState({value: this.state.value + 1});
-    }
 
     render() {
-
-        console.log("props", this.props)
-
         return (
             <div>
+                <h4>{this.props.id}</h4>
                 <span className={this.newMethod()}> {this.formatCount()} </span> 
                 <button 
-                    onClick={ (product) => this.handleIncrement(product)} 
+                    onClick={() => this.props.onIncrement(this.props.counter)} 
                     className="btn btn-secondary btn-sm"
-                >
-                    Increment
+                    > Increment 
                 </button>
-               
+                <button 
+                    className="btn-danger btn-sm m-2"
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    >Delete
+                </button>
             </div>
         );
     }
 
     newMethod() {
         let classes = "badge m-2 badge-";
-        classes += this.state.value === 0 ? "warning" : "primary";
+        classes += this.props.counter.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const {value} = this.state
+        const {value} = this.props.counter;
         return value === 0 ? 'Zero' : value;
     }
 }
